0ae372e2a9429be8e61f8ae2d952f630e52333b4
lone-do <Lone-DO@users.noreply.github.com>
2017-01-12 15:56:27 -0500

    Refactoring Finalized code

--- C#/FitnessFrog/FitnessFrog/Program.cs
+++ C#/FitnessFrog/FitnessFrog/Program.cs
@@ -5,66 +5,61 @@ namespace Treehouse.FitnessFrog
     {
         static void Main(string[] args)
         {
-            double runningTotal = 0;
+            var runningTotal = 0.0;
 
-            bool excersising = true;
-            while (excersising)
+            while (true)
             {
                 // Prompt user for minutes exercised 
                 Console.Write("Enter how many minutes you excersised or type \"quit\" to stop: ");
-                string entry = Console.ReadLine();
+                var entry = Console.ReadLine();
 
                 if (entry.ToLower() == "quit")
                 {
-                    excersising = !excersising;
+                    break;
                 }
-                else
+                try
                 {
-                    try
+                    // Add minutes exercised to total 
+                    var minutes = double.Parse(entry);
+
+                    if (minutes <= 0)
                     {
-                        // Add minutes exercised to total 
-                        double minutes = double.Parse(entry);
-                        
-                         
-                        if (minutes <= 0)
-                        {
-                            Console.WriteLine(minutes + " is not an acceptable value.");
-                            continue;
-                        }
-                        else if (minutes <= 10)
-                        {
-                            Console.WriteLine("Keep it up!");
-                        }
-                        else if (minutes <= 20)
-                        {
-                            Console.WriteLine("Way to go buddy!");
-                        }
-                        else if (minutes <= 35)
-                        {
-                            Console.WriteLine("You are on Fire today!");
-                        }
-                        else if (minutes <= 60)
-                        {
-                            Console.WriteLine("Someone is prepping for the League!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("To the Championships with you!");
-                        }
-                        runningTotal += minutes;
-                    }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("That is not valid input.");
+                        Console.WriteLine(minutes + " is not an acceptable value.");
                         continue;
                     }
+                    else if (minutes <= 10)
+                    {
+                        Console.WriteLine("Keep it up!");
+                    }
+                    else if (minutes <= 20)
+                    {
+                        Console.WriteLine("Way to go buddy!");
+                    }
+                    else if (minutes <= 35)
+                    {
+                        Console.WriteLine("You are on Fire today!");
+                    }
+                    else if (minutes <= 60)
+                    {
+                        Console.WriteLine("Someone is prepping for the League!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("To the Championships with you!");
+                    }
+                    runningTotal += minutes;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("That is not valid input.");
+                    continue;
+                }
 
-                    // Display total minutes exercised to the screen 
-                    Console.WriteLine("You've entered " + runningTotal + " minutes.");
-
-                } 
+                // Display total minutes exercised to the screen 
+                Console.WriteLine("You've entered " + runningTotal + " minutes.");
                 // Repeat until user quits
             }
+            Console.WriteLine("Goodbye");
         }
     }
 }
