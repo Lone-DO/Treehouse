84613d8ba90c0d397db09e8a09b7bb18bddbc121
lone-do <Lone-DO@users.noreply.github.com>
2017-01-12 13:19:17 -0500

    Input control options

--- C#/FitnessFrog/FitnessFrog/Program.cs
+++ C#/FitnessFrog/FitnessFrog/Program.cs
@@ -14,38 +14,57 @@ namespace Treehouse.FitnessFrog
                 Console.Write("Enter how many minutes you excersised or type \"quit\" to stop: ");
                 string entry = Console.ReadLine();
                 entry = entry.ToLower();
+
                 if (entry == "quit")
                 {
                     excersising = !excersising;
                 }
                 else
                 {
-                    // Add minutes exercised to total 
-                    int minutes = int.Parse(entry);
-                    runningTotal += minutes;
+                    try
+                    {
+                        // Add minutes exercised to total 
+                        int minutes = int.Parse(entry);
+                        
+                         
+                        if (minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not an acceptable value.");
+                            continue;
+                        }
+                        else if (minutes <= 10)
+                        {
+                            Console.WriteLine("Keep it up!");
+                        }
+                        else if (minutes <= 20)
+                        {
+                            Console.WriteLine("Way to go buddy!");
+                        }
+                        else if (minutes <= 35)
+                        {
+                            Console.WriteLine("You are on Fire today!");
+                        }
+                        else if (minutes <= 60)
+                        {
+                            Console.WriteLine("Someone is prepping for the League!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("To the Championships with you!");
+                        }
+                        runningTotal += minutes;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("That is not valid input.");
+                        continue;
+                    }
+
                     // Display total minutes exercised to the screen 
-                    if (runningTotal <= 10 )
-                    {
-                        Console.WriteLine("You've excersised " + runningTotal + " minutes");
-                    }
-                    else if (runningTotal <= 20)
-                    {
-                        Console.WriteLine("You've excersised " + runningTotal + " minutes. Way to go buddy!");
-                    }
-                    else if (runningTotal <= 35)
-                    {
-                        Console.WriteLine("You've excersised " + runningTotal + " minutes, You are on Fire today!");
-                    }
-                    else if (runningTotal <= 60)
-                    {
-                        Console.WriteLine("You've excersised " + runningTotal + " minutes, Someone is prepping for the League!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("To the Championships with you!");
-                    }
-                    // Repeat until user quits
-                }
+                    Console.WriteLine("You've entered " + runningTotal + " minutes.");
+
+                } 
+                // Repeat until user quits
             }
         }
     }
