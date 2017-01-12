b207f883563b916e4ae9acaaa8f3c2365d15f0b2
lone-do <Lone-DO@users.noreply.github.com>
2017-01-12 12:07:58 -0500

    Add Else if Clauses to the application

--- C#/FitnessFrog/FitnessFrog/Program.cs
+++ C#/FitnessFrog/FitnessFrog/Program.cs
@@ -5,16 +5,48 @@ namespace Treehouse.FitnessFrog
     {
         static void Main(string[] args)
         {
-            // Prompt user for minutes exercised 
-            Console.Write("Enter how many minutes you excersised: ");
+            int runningTotal = 0;
 
-            string entry = System.Console.ReadLine();
-
-            // Add minutes exercised to total 
-
-            // Display total minutes exercised to the screen 
-            Console.WriteLine("You've excersised " + entry + " minutes");
-            // Repeat until user quits 
+            bool excersising = true;
+            while (excersising)
+            {
+                // Prompt user for minutes exercised 
+                Console.Write("Enter how many minutes you excersised or type \"quit\" to stop: ");
+                string entry = Console.ReadLine();
+                entry = entry.ToLower();
+                if (entry == "quit")
+                {
+                    excersising = !excersising;
+                }
+                else
+                {
+                    // Add minutes exercised to total 
+                    int minutes = int.Parse(entry);
+                    runningTotal += minutes;
+                    // Display total minutes exercised to the screen 
+                    if (runningTotal <= 10 )
+                    {
+                        Console.WriteLine("You've excersised " + runningTotal + " minutes");
+                    }
+                    else if (runningTotal <= 20)
+                    {
+                        Console.WriteLine("You've excersised " + runningTotal + " minutes. Way to go buddy!");
+                    }
+                    else if (runningTotal <= 35)
+                    {
+                        Console.WriteLine("You've excersised " + runningTotal + " minutes, You are on Fire today!");
+                    }
+                    else if (runningTotal <= 60)
+                    {
+                        Console.WriteLine("You've excersised " + runningTotal + " minutes, Someone is prepping for the League!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("To the Championships with you!");
+                    }
+                    // Repeat until user quits
+                }
+            }
         }
     }
 }
