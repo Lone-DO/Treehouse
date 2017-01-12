5330b6f4cc34bf644965395510ed171cc890316a
lone-do <Lone-DO@users.noreply.github.com>
2017-01-12 16:16:12 -0500

    Completed C# Basics Code Challenge,
    "Repeatedly prompt the user for numbers. Add all of the numbers together. When the user types in “done”, print the average of all of the numbers by dividing the total by the number of numbers entered."

---
+++ C#/AveragerCodeChallenge/AveragerCodeChallenge/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace AveragerCodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculate = 0.0;
+            var i = 0;
+
+            while (true)
+            {
+                // Prompt user for numbers
+                Console.Write("Enter a number or type \"done\" to see the average: ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "done")
+                {
+                    // Display Average Number of all inputs
+                    Console.WriteLine("Average: " + (calculate / i));
+                    break;
+                }
+                try
+                {
+                    // Add inputs to Calculate Array
+                    var number = double.Parse(input);
+
+                    if (number <= 0)
+                    {
+                        Console.WriteLine(number + " is not an acceptable value.");
+                        continue;
+                    }
+                    calculate += number;
+                    i += 1;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("That is not valid input.");
+                    continue;
+                }
+                // Repeat until user quits
+            }
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
