0d14981ea43a297010b5cb6e49dea4e9d47748f7
lone-do <Lone-DO@users.noreply.github.com>
2017-02-01 10:36:51 -0500

    Revert "Revert "testing""
    This reverts commit 74e04c6a4635fd271da5a6b41786bb8ab72470b3.

---
+++ C#/MyDataTypes/MyDataTypes/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("MyDataTypes")>
+<Assembly: AssemblyProduct("MyDataTypes")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type. Only Windows
+' assemblies support COM.
+<Assembly: ComVisible(False)>
+
+' On Windows, the following GUID is for the ID of the typelib if this
+' project is exposed to COM. On other platforms, it unique identifies the
+' title storage container when deploying this assembly to the device.
+<Assembly: Guid("b3a33b06-8ee0-48bc-9031-14a0c3291f63")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+<Assembly: AssemblyVersion("1.0.0.0")>
