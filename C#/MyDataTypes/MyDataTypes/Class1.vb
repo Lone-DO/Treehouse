d11a973ceaa6363656317db18c94c61cab4766db
lone-do <Lone-DO@users.noreply.github.com>
2017-02-01 10:31:36 -0500

    testing

---
+++ C#/MyDataTypes/MyDataTypes/Class1.vb
@@ -0,0 +1,10 @@
+Namespace MyDataTypes
+{
+    Public Class PetData
+    {
+        Public String Name;
+        Public String Species;
+        Public float Weight;
+        Public int Age;
+    }
+}
