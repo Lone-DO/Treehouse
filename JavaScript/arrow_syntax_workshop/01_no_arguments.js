c9fdc24a31225ec28bcba5f6cf6a2f9f1a07268b
lone-do <lotussythe@yahoo.com>
2017-05-30 08:18:27 -0400

    Concise and new Arrow syntax practice

--- JavaScript/arrow_syntax_workshop/01_no_arguments.js
+++ JavaScript/arrow_syntax_workshop/01_no_arguments.js
@@ -1,10 +1,8 @@
 const name = "Andrew";
 
-function sayName() {
+const sayName = () => {
     const message = "My name is " + name;
     console.log(message);
 }
 
-function sayBye() {
-  console.log("Bye " + name);  
-}
+const sayBye = () => console.log("Bye " + name);
