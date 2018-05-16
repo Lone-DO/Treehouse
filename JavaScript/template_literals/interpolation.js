0af99a7c8a74d8e8bcf0190a8914f968f012289d
lone-do <Lone-DO@users.noreply.github.com>
2018-05-15 22:58:45 -0400

    Arhiving/ Restarting Course

--- JavaScript/template_literals/interpolation.js
+++
@@ -1,7 +0,0 @@
-const like = thing => `I like ${thing}`;
-
-const love = thing => `I love ${thing}`;
-
-const sentence = `<p>${like('apples')}, but ${love('oranges')}</p>`;
-
-document.querySelector('.interpolation').innerHTML = sentence;
