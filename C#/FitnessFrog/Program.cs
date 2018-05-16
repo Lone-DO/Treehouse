3ca748318200a662f76bc4ffd2af30cb9e56570a
lone-do <lotussythe@gmail.com>
2018-05-16 06:50:17 -0400

    staging C# Enviroment

---
+++ C#/FitnessFrog/Program.cs
@@ -0,0 +1,9 @@
+class App
+{
+    static void Main() {
+       //Prompt user for minutes excersised
+       //Add minutes to total
+       //Display total minutes excersised
+       //Loop till user quits
+    }
+}
