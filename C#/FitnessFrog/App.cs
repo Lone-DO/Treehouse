b275eded145c87efb72aefc63a8fc1c5ccedb8d7
lone-do <Lone-DO@users.noreply.github.com>
2018-05-16 12:01:48 -0400

    Feedback on user progression, Loop app until input to quit

---
+++ C#/FitnessFrog/App.cs
@@ -0,0 +1,62 @@
+using System;
+
+namespace Csharp.FitnessFrog
+{
+   class App
+   {
+      static void Main()
+      {
+         int workoutTotal = 0;
+         bool keepGoing = true;
+
+         while (keepGoing == true)
+         {
+            //Prompt user for minutes excersised
+            Console.Write("Enter how many minutes you excercised, or enter 'quit' to stop the application: ");
+
+            string entry = Console.ReadLine();
+
+            //Loop till user quits
+            if (entry == "quit")
+            {
+               keepGoing = false;
+            }
+            else
+            {
+               int time = int.Parse(entry);
+
+               if (time <= 0)
+               {
+                  Console.WriteLine(time + " is not an acceptable value");
+                  continue;
+               }
+               else if (time <= 10)
+               {
+                  Console.WriteLine("Way to go hot stuff");
+               }
+               else if (time <= 30)
+               {
+                  Console.WriteLine("You must be a ninja warrior in training");
+               }
+               else if (time <= 60)
+               {
+                  Console.WriteLine("Aiming for 1st place");
+               }
+               else
+               {
+                  Console.WriteLine("Now you are just showing off...");
+               }
+
+               //Add minutes to total
+               workoutTotal += time;
+
+               //Display total minutes excersised
+               Console.WriteLine("You've Excersised for " + workoutTotal + " minutes.");
+            }
+
+         }
+         Console.Write("Goodbye");
+
+      }
+   }
+}
--- C#/FitnessFrog/Program.cs
+++
@@ -1,9 +0,0 @@
-class App
-{
-    static void Main() {
-       //Prompt user for minutes excersised
-       //Add minutes to total
-       //Display total minutes excersised
-       //Loop till user quits
-    }
-}
