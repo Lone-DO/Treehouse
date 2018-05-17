573f2d7054fcc221016b6c7465ecc080302168d8
lone-do <Lone-DO@users.noreply.github.com>
2018-05-17 00:56:42 -0400

    Setting Csharp Mono Enviroment

--- C#/FitnessFrog/App.cs
+++ C#/FitnessFrog/App.cs
@@ -2,67 +2,67 @@ using System;
 
 namespace Csharp.FitnessFrog
 {
-   class App
-   {
-      static void Main()
-      {
-         double workoutTotal = 0;
+    class App
+    {
+        static void Main()
+        {
+            double workoutTotal = 0;
 
-         while (true)
-         {
-            //Prompt user for minutes excersised
-            Console.WriteLine("Enter how many minutes you excercised, or enter 'quit' to stop the application: ");
-
-            string entry = Console.ReadLine();
-
-            try
+            while (true)
             {
-               //Loop till user quits
-               if (entry.ToLower() == "quit")
-               {
-                  break;
-               }
+                //Prompt user for minutes excersised
+                Console.WriteLine("Enter how many minutes you excercised, or enter 'quit' to stop the application: ");
 
-               double time = double.Parse(entry);
+                string entry = Console.ReadLine();
 
-               if (time <= 0)
-               {
-                  Console.WriteLine(time + " is not an acceptable value");
-                  continue;
-               }
-               else if (time <= 10)
-               {
-                  Console.WriteLine("Way to go hot stuff");
-               }
-               else if (time <= 30)
-               {
-                  Console.WriteLine("You must be a ninja warrior in training");
-               }
-               else if (time <= 60)
-               {
-                  Console.WriteLine("Aiming for 1st place");
-               }
-               else
-               {
-                  Console.WriteLine("Now you are just showing off...");
-               }
+                try
+                {
+                    //Loop till user quits
+                    if (entry.ToLower() == "quit")
+                    {
+                        break;
+                    }
 
-               //Add minutes to total
-               workoutTotal += time;
+                    double time = double.Parse(entry);
 
-               //Display total minutes excersised
-               Console.WriteLine("You've Excersised for " + workoutTotal + " minutes.");
+                    if (time <= 0)
+                    {
+                        Console.WriteLine(time + " is not an acceptable value");
+                        continue;
+                    }
+                    else if (time <= 10)
+                    {
+                        Console.WriteLine("Way to go hot stuff");
+                    }
+                    else if (time <= 30)
+                    {
+                        Console.WriteLine("You must be a ninja warrior in training");
+                    }
+                    else if (time <= 60)
+                    {
+                        Console.WriteLine("Aiming for 1st place");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Now you are just showing off...");
+                    }
 
+                    //Add minutes to total
+                    workoutTotal += time;
+
+                    //Display total minutes excersised
+                    Console.WriteLine("You've Excersised for " + workoutTotal + " minutes.");
+
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Input is invalid, please input a number...");
+                }
 
             }
-            catch (FormatException)
-            {
-               Console.WriteLine("Input is invalid, please input a number...");
-            }
-
-         }
-         Console.WriteLine("Goodbye, Press any key to Close!");
-         Console.ReadKey();
-      }
-   }
+            Console.WriteLine("Goodbye, Press any key to Close!");
+            Console.ReadKey();
+        }
+    }
 }
