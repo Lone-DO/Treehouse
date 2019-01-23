f6d76a5fc29f276f46ef381660919a447aab1321
lone-do <lotussythe@gmail.com>
2019-01-23 17:40:51 -0500

    Practice: Solved

--- FSFoundations/Python/madlibs.py
+++ FSFoundations/Python/madlibs.py
@@ -8,5 +8,12 @@
 
 # TODO: Prompt the user for parts of speech and store them in variables
 
+verb = input("Can you give me a Verb? ")
+noun = input("Great, now how about a Noun? ")
+adjective = input("And finally, can you think of an Adjective? ")
 
 # TODO: Output the template to the screen with the blanks filled out with what the user stated
+print(" ")  # adds gutter space between questions and output
+print("I enjoy practice! I find it helps me to", verb, "better.")
+print("Without practice, my", noun, "would problably not even work.")
+print("My code is getting more", adjective, "every single day!")
