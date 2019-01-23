cdddab8efdda7b140b40be360f20bd814a0ce8e1
lone-do <lotussythe@gmail.com>
2019-01-23 17:11:50 -0500

    Python basics

---
+++ FSFoundations/Python/hello.py
@@ -0,0 +1,9 @@
+# Syntax formatting notes for python
+snake_case = "for Variables, methods, & functions"
+camelCase = "camelCase is legacy, don't use for Python"
+StudlyCaps = "StudlyCaps is meant for ClassNames in Python"
+CONSTANTS = "Constants should be written IN_ALL_CAPS in Python."
+# __________________________________________________________________
+
+first_name = "Lone"
+print(first_name, "is learning python")
