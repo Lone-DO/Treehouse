e613057424199dcf1441cb48a8ea92ad8061a8d0
lone-do <lotussythe@gmail.com>
2019-01-23 17:31:30 -0500

    Learning inputs

--- FSFoundations/Python/hello.py
+++ FSFoundations/Python/hello.py
@@ -1,9 +1,13 @@
-# Syntax formatting notes for python
+# Syntax formatting notes for python(3)
 snake_case = "for Variables, methods, & functions"
 camelCase = "camelCase is legacy, don't use for Python"
 StudlyCaps = "StudlyCaps is meant for ClassNames in Python"
 CONSTANTS = "Constants should be written IN_ALL_CAPS in Python."
 # __________________________________________________________________
 
-first_name = "Lone"
+first_name = input("What is your name? ")
+print("Guten tag,", first_name)
 print(first_name, "is learning python")
+
+favorite_color = input("What is your favorite color? ")
+print("The color", favorite_color, "is a excellent color")
