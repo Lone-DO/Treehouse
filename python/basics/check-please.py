9f1c5047553a66ad8636654b83cbba1b3c528f2d
lone-do <lotussythe@gmail.com>
2020-05-12 19:09:47 -0400

    try, catch, else, raise, ValueError

--- python/basics/check-please.py
+++ python/basics/check-please.py
@@ -2,11 +2,26 @@ import math
 
 
 def split_check(total, num_of_ppl):
+    if num_of_ppl < 1:
+        raise ValueError("At least one person is required to split the check")
+    if total <= 0:
+        raise ValueError("Bill cannot be negative, Please try again!")
+    elif total = 0:
+        return "nothing, this order was on the house!"
     return math.ceil(total / num_of_ppl)
 
 
-total_due = float(input("What is the total? "))
-num_of_ppl = float(input("How many people? "))
+def pay():
+    try:
+        total_due = float(input("What is the total? "))
+        num_of_ppl = float(input("How many people? "))
+        amount_due = split_check(total_due, num_of_ppl)
+    except Exception as err:
+        print("Invalid Input, Please try again!")
+        print("({})".format(err))
+        pay()
+    else:
+        print("Each person owes ${}".format(amount_due))
 
-amount_due = split_check(total_due, num_of_ppl)
-print("Each person owes ${}".format(amount_due))
+
+pay()
