b92c7be9b9fbe0dab90389fad38bc7f5741bd5c8
lone-do <lotussythe@gmail.com>
2020-05-13 20:45:20 -0400

    file for notes

---
+++ C#/Basics/Notes.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+namespace Basics
+{
+    class Notes
+    {
+        static void Primary(string[] args)
+        {
+            Wait();
+            Variables();
+        }
+        static void Variables()
+        {
+            int wholeNumber = 4;
+            double pi = 3.14;
+            bool status = true;
+            string greeting = "Hello world";
+            char letter = 'z';
+
+            Console.WriteLine(wholeNumber.GetType());
+            Console.WriteLine(pi.GetType());
+            Console.WriteLine(status.GetType());
+            Console.WriteLine(greeting.GetType());
+            Console.WriteLine(letter.GetType());
+        }
+        static void Wait()
+        {
+            Console.WriteLine("Waiting...");
+            Thread.Sleep(3000);
+            Console.WriteLine("Done");
+        }
+        static void CountToThree()
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.WriteLine(3);
+        }
+    }
+}
--- C#/Basics/Program.cs
+++ C#/Basics/Program.cs
@@ -7,21 +7,6 @@ namespace Basics
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-        }
-        static void Variables()
-        {
-            int wholeNumber = 4;
-            double pi = 3.14;
-            bool status = true;
-            string greeting = "Hello world";
-            char letter = 'z';
-
-            System.Console.WriteLine(wholeNumber.GetType());
-            System.Console.WriteLine(pi.GetType());
-            System.Console.WriteLine(status.GetType());
-            System.Console.WriteLine(greeting.GetType());
-            System.Console.WriteLine(letter.GetType());
         }
     }
 }
