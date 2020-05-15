6558ed1d8d36f4de63402547fbf0df9a70460638
lone-do <lotussythe@gmail.com>
2020-05-15 11:50:24 -0400

    Ticketing widget, TODO: Refactor?

--- python/basics/masterticket.py
+++ python/basics/masterticket.py
@@ -4,29 +4,52 @@ exit = False
 
 # Output how many tickets are remaining using the tickets_remaining variable
 
+
+def Quantity(customer):
+    tickets = input("How many tickets do you want, {}? ".format(customer))
+    if tickets == "0" or tickets == "quit":
+        return "cancelled"
+    elif tickets.isnumeric():
+        return int(tickets)
+    else:
+        print("Error, Invalid Input")
+        return Quantity(customer)
+
+
+def Sum(amount, cost):
+    return amount * cost
+
+
+def Checkout(amount, total, stock):
+    confirm = input(
+        "You have ordered {} tickets, your total will be ${}. Please confirm (Enter: yes/no) ".format(amount, total))
+    if confirm.lower() == "yes":
+        if tickets <= stock:
+            print("Thank you for your order {}".format(name))
+            return stock - tickets
+        else:
+            print("Error, Cannot process order over available tickets")
+            return stock
+    else:
+        print("Order has been cancelled, Have a great day!")
+        return stock
+
+
 while tickets_remaining > 0 and not exit:
     print("There are {} tickets remaining!".format(tickets_remaining))
     # Gather Users name and sign it to new variable
     name = input("What is your name? ")
-    # Prompt user by name and ask how many
-    num_tickets = input(
-        "Hello {}, how many tickets do you want? ".format(name))
-    if num_tickets == "0" or num_tickets == "quit":
+    # Prompt user by name and ask how many tickets
+    tickets = Quantity(name)
+    if tickets == "cancelled":
         exit = True
     else:
-        num_tickets = int(num_tickets)
         # Calculate the price (amount * price)
-        total = TICKET_PRICE * num_tickets
+        total = Sum(tickets, TICKET_PRICE)
         # Output the price to the screen
-        confirm = input(
-            "You have ordered {} tickets, your total will be ${}. Please confirm (Enter: yes/no) ".format(num_tickets, total))
-        if confirm.lower() == "yes":
-            if num_tickets <= tickets_remaining:
-                tickets_remaining -= num_tickets
-                print("Thank you for your order {}".format(name))
-            else:
-                print("Error, Cannot process order over available tickets")
-        else:
-            print("Order has been cancelled, Have a great day!")
-if tickets_remaining:
+        tickets_remaining = Checkout(tickets, total, tickets_remaining)
+
+if not tickets_remaining:
     print("SOLD OUT!")
+else:
+    print("Exiting Program... GOODBYE!")
