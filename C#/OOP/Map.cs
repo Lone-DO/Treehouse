01a97edc680a79df9241ef9cd1b65f6378ebc82f
lone-do <lotussythe@gmail.com>
2020-05-22 16:55:41 -0400

    Returning values

--- C#/OOP/Map.cs
+++ C#/OOP/Map.cs
@@ -2,16 +2,23 @@ namespace TreehousDefense
 {
     class Map
     {
-        public void Main()
-        {
-
-        }
         public readonly int Width;
         public readonly int Height;
         public Map(int width, int height)
         {
             Width = width;
             Height = height;
+        }
+
+        public bool OnMap(Point point)
+        {
+            bool validX = point.X >= 0 && point.X <= Width;
+            bool validY = point.Y >= 0 && point.Y <= Height;
+
+            bool inbound = validX && validY;
+            bool outbound = !inbound;
+
+            return inbound;
         }
     }
 
