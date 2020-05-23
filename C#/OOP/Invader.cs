043116b479e6c48ba5e203e37eed0e7f935708b3
lone-do <lotussythe@gmail.com>
2020-05-23 16:23:24 -0400

    Computed Properties

--- C#/OOP/Invader.cs
+++ C#/OOP/Invader.cs
@@ -2,7 +2,24 @@ namespace TreehousDefense
 {
     class Invader
     {
+        private int _pathStep = 0;
+        private readonly Path _path;
+        public MapLocation Location
+        {
+            get
+            {
+                return _path.GetLocationAt(_pathStep);
+            }
+        }
 
-        public MapLocation Location { get; set; }
+        public Invader(Path path)
+        {
+            _path = path;
+        }
+
+        public void Move()
+        {
+            _pathStep++;
+        }
     }
 }
