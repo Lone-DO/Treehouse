d1c9afcfe7ff5afce79c8daa517a0e09c18d8969
lone-do <lotussythe@gmail.com>
2020-05-23 18:12:01 -0400

    finalized Tower Defense game,

--- C#/OOP/Tower.cs
+++ C#/OOP/Tower.cs
@@ -1,9 +1,13 @@
+using System;
+
 namespace TreehousDefense
 {
     class Tower
     {
         private const int _power = 1;
         private const int _range = 1;
+        private const double _accuracy = .75;
+        private static readonly Random _random = new Random();
         private readonly MapLocation _location;
 
         public Tower(MapLocation location)
@@ -11,14 +15,21 @@ namespace TreehousDefense
             // Add validation so Tower isnt placed on Path
             _location = location;
         }
-
+        public bool IsSuccessfulShot()
+        {
+            return _random.NextDouble() < _accuracy;
+        }
         public void FireOnInvaders(Invader[] invaders)
         {
             foreach (Invader invader in invaders)
             {
                 if (invader.IsActive && _location.InRangeOf(invader.Location, _range))
                 {
-                    invader.TakeDamage(_power);
+                    if (IsSuccessfulShot())
+                    {
+                        invader.TakeDamage(_power);
+                        Console.WriteLine("Hit Confirmed");
+                    }
                     break;
                 }
             }
