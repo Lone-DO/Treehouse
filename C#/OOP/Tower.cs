17b199d0177bc15d7f7a7a5c63a592cb0a475c0b
lone-do <lotussythe@gmail.com>
2020-05-23 16:57:04 -0400

    Loops, Setting Towers, Tracking Invaders

--- C#/OOP/Tower.cs
+++ C#/OOP/Tower.cs
@@ -2,9 +2,24 @@ namespace TreehousDefense
 {
     class Tower
     {
-        public void Main()
-        {
+        private readonly MapLocation _location;
 
+        public Tower(MapLocation location)
+        {
+            // Add validation so Tower isnt placed on Path
+            _location = location;
+        }
+
+        public void FireOnInvaders(Invader[] invaders)
+        {
+            int index = 0;
+            while (index < invaders.Length)
+            {
+                Invader invader = invaders[index];
+                // Do stuff to invader
+
+                index++;
+            }
         }
     }
 }
