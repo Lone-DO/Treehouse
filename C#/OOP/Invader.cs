8adba02479b60649a938ebced6a05418b995cd8b
lone-do <lotussythe@gmail.com>
2020-05-23 16:44:33 -0400

    Encapsulation, Sugar Syntax,

--- C#/OOP/Invader.cs
+++ C#/OOP/Invader.cs
@@ -3,23 +3,14 @@ namespace TreehousDefense
     class Invader
     {
         private int _pathStep = 0;
+        public int Health { get; private set; } = 2;
         private readonly Path _path;
-        public MapLocation Location
-        {
-            get
-            {
-                return _path.GetLocationAt(_pathStep);
-            }
-        }
-
-        public Invader(Path path)
-        {
-            _path = path;
-        }
-
-        public void Move()
-        {
-            _pathStep++;
-        }
+        public MapLocation Location => _path.GetLocationAt(_pathStep);
+        public Invader(Path path) => _path = path;
+        public bool HasScored => _pathStep >= _path.Length;
+        public bool IsNeutralized => Health <= 0;
+        public bool IsActive => !(IsNeutralized || HasScored);
+        public void Move() => _pathStep++;
+        public void TakeDamage(int factor) => Health -= factor;
     }
 }
--- C#/OOP/Path.cs
+++ C#/OOP/Path.cs
@@ -3,6 +3,7 @@ namespace TreehousDefense
     class Path
     {
         private readonly MapLocation[] _path;
+        public int Length => _path.Length;
         public Path(MapLocation[] path)
         {
             _path = path;
