6762f2fa1768f2fb7ae6888304aedd369040629f
lone-do <lotussythe@gmail.com>
2020-06-14 15:12:52 -0400

    Updating Python list env

---
+++ python/list/shopping_list.py
@@ -0,0 +1,20 @@
+shopping_list = []
+
+
+def show_help():
+    print("What should we pick up at the store?")
+    print("""
+Enter 'DONE' to stop adding items.
+Enter 'HELP' for this help.
+""")
+
+
+show_help()
+while True:
+    new_item = input("> ")
+
+    if new_item.upper() == 'DONE':
+        break
+    elif new_item == 'HELP':
+        show_help()
+        continue
