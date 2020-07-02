552a56d840e0e45845da48c7bf11e73407a27019
lone-do <lotussythe@gmail.com>
2020-07-02 02:10:06 -0400

    Treehouse Code Practice, Initial env

---
+++ C#/Steams and Data/SoccerStats/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace SoccerStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDir = Directory.GetCurrentDirectory();
+            var directory = new DirectoryInfo(currentDir);
+            var files = directory.GetFiles("*.txt");
+            foreach(var file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
+        }
+    }
+}
