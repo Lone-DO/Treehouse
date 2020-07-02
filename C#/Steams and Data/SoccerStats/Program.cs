d4db0da811acaca5b9a284cacd49517fad95b1e9
lone-do <lotussythe@gmail.com>
2020-07-02 02:53:41 -0400

    Using Statements with StreamReaders

--- C#/Steams and Data/SoccerStats/Program.cs
+++ C#/Steams and Data/SoccerStats/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Xml.Xsl;
 
 namespace SoccerStats
 {
@@ -9,10 +10,16 @@ namespace SoccerStats
         {
             string currentDir = Directory.GetCurrentDirectory();
             var directory = new DirectoryInfo(currentDir);
-            var files = directory.GetFiles("*.txt");
-            foreach(var file in files)
-            {
-                Console.WriteLine(file.Name);
+            var filename = Path.Combine(directory.FullName, "data.txt");
+            var file = new FileInfo(filename);
+            if (file.Exists)
+            { 
+                using (var reader = new StreamReader(file.FullName))
+                {
+                    Console.SetIn(reader);
+                    Console.WriteLine(Console.ReadLine());
+                }
+                Console.ReadLine();
             }
         }
     }
