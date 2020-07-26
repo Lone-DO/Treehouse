f8c6cbdde5e124764b3a1d1ee86203375d646fd3
lone-do <lotussythe@gmail.com>
2020-07-26 14:52:21 -0400

    Created IEnumerable Generic Class

---
+++ C#/DataStruct/EnumberableCompositor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericsDemo
+{
+    class EnumberableCompositor<T> : IEnumerable<T>
+    {
+        private List<IEnumerable<T>> _collections;
+
+        public void Add(IEnumerable<T> collection)
+        {
+            _collections.Add(collection);
+        }
+
+        public EnumberableCompositor()
+        {
+            _collections = new List<IEnumerable<T>>();
+        }
+        public EnumberableCompositor(IEnumerable<IEnumerable<T>> collections)
+        {
+            _collections = collections.ToList();
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- C#/DataStruct/Program.cs
+++ C#/DataStruct/Program.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -5,23 +6,32 @@ namespace GenericsDemo
 {
     class Program
     {
-        public static bool IsOdd(int value)
+        static void Main(string[] args)
         {
-            return value % 2 != 0;
-        }
-
-        public void Main(string[] args)
-        {
+            Console.WriteLine("Hello World!");
             var list1 = new List<int> { 1, 2, 3, 4, 5 };
             var list2 = new List<int> { 2, 4, 6, 8, 10 };
             var set1 = new HashSet<int> { 3, 6, 9, 12, 15 };
             var array1 = new[] { 4, 8, 12, 16, 20 };
 
-            int numOdd =
-                list1.Count(x => IsOdd(x)) +
-                list2.Count(x => IsOdd(x)) +
-                set1.Count(x => IsOdd(x)) +
-                array1.Count(x => IsOdd(x));
+
+            var ec = new EnumberableCompositor<int> { list1, list2, set1, array1 };
+            //int numOdd = 0;
+
+            //foreach(var value in ec)
+            //{
+            //    if (IsOdd(value))
+            //    {
+            //        numOdd++;
+            //    }
+            //}
+
+            //int numOdd = ec.Count(x => IsOdd(x));
+        }
+
+        public static bool IsOdd(int value)
+        {
+            return value % 2 != 0;
         }
     }
 }
